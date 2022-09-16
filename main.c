92d8275fe9e9f44bdd9de517ba680c6ff3528888
De Oliveira <68508920+pedrodeoliveiraa@users.noreply.github.com>
2022-09-15 21:55:30 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,8 @@
+// O programa deve receber e mostrar notas, após calcular e exibir médias.
+
+void main() {
+    float nota1, nota2, nota3, media;
+    printf ("Digite a nota 1: ")
+    scanf ("%f", &nota1);
+
+}
