8c8a4ac68c25b1cbd4259d2210b3c1d4ec38ba55
De Oliveira <68508920+pedrodeoliveiraa@users.noreply.github.com>
2022-09-15 21:56:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
-// O programa deve receber e mostrar notas, após calcular e exibir médias.
+#include <stdio.h>
 
-void main() {
-    float nota1, nota2, nota3, media;
-    printf ("Digite a nota 1: ")
-    scanf ("%f", &nota1);
+void main(){
+    float nota1,nota2,nota3, media;
 
+    printf("digite a nota 1: ");
+    scanf("%f", &nota1);
+
+    printf("digite a nota2: ");
+    scanf("%f", &nota2);
+
+    printf("digite a nota3: ");
+    scanf("%f", &nota3);
+
+    media =(nota1 + nota2 + nota3)/ 3;
+    printf("media final: %2f", media);
 }
